6c87fc8159981cc00b551b7f1e67567c2aa11568
paul wisehart <paul@oldcode.org>
2012-05-13 20:32:46 -0400

    add a script ot grab the jars, (and fake a gui browser to the mvn repo).

---
+++ get-jars.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd lib/
+wget \
+    -U 'IE6 on Windows XP: Mozilla/4.0 (compatible; MSIE 6.0; Microsoft Windows NT 5.1)' \
+    http://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.1/commons-lang3-3.1.jar
