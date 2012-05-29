137ec5a39d4359c716f9b560c22f5aa000c485bf
paul wisehart <paul@oldcode.org>
2012-05-28 21:35:20 -0400

    asd

--- get-jars.sh
+++ get-jars.sh
@@ -5,4 +5,4 @@ wget \
     -U 'IE6 on Windows XP: Mozilla/4.0 (compatible; MSIE 6.0; Microsoft Windows NT 5.1)' \
     http://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.1/commons-lang3-3.1.jar
 
-wget 'http://files.zentus.com/sqlitejdbc/sqlitejdbc-v056.jar'
+#wget 'http://files.zentus.com/sqlitejdbc/sqlitejdbc-v056.jar'
